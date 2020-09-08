6ff62bc5346df4de5faa4c80ad197d862fa4f2f2
Yeri <arifyeripratama@gmail.com>
2020-09-09 00:51:29 +0700

    change favicon path

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,7 +67,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/favicon.png`,
+        icon: `static/favicon.png`,
       },
     },
     `gatsby-plugin-react-helmet`,
