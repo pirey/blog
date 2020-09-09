20f6957ab1b47344d121fabf6d236381abc244fa
Yeri <arifyeripratama@gmail.com>
2020-09-09 10:01:03 +0700

    change home page title

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ const BlogIndex = ({ data, location }) => {
 
   return (
     <Layout location={location} title={siteTitle}>
-      <SEO title="All posts" />
+      <SEO title="Yeri's interpretations on various things." />
       {posts.map(({ node }) => {
         const title = node.frontmatter.title || node.fields.slug
         return (
