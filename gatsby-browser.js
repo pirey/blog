827082b897e4dc84439d195e0c3ef9f0dd9a2fb7
Yeri <yeri@accelbyte.net>
2023-07-30 18:57:41 +0700

    content: customizing telescope

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,7 +2,8 @@
 import "typeface-montserrat"
 import "typeface-merriweather"
 
-import "./static/prism-nord.css"
+// import "./static/prism-nord.css"
+import "./static/prism-coldark-cold.css"
 import "prismjs/plugins/command-line/prism-command-line.css"
 
 import "./src/styles/global.css"
